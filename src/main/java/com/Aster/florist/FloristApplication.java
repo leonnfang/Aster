6f9feb7a7b3af9e8a5b40370f8fc922a9fd74adf
Leon Fang <fang@cooper.edu>
2020-02-19 18:27:13 -0500

    update the new initilation files

---
+++ src/main/java/com/Aster/florist/FloristApplication.java
@@ -0,0 +1,15 @@
+package com.Aster.florist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloristApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FloristApplication.class, args);
+		System.out.println("Welcome to Aster");
+	}
+
+}
