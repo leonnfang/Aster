8337e4964d2438448f3ba9385e16aa7626f8ee51
Leon Fang <fang@cooper.edu>
2020-02-28 15:41:11 -0500

    "minor updates"

--- src/main/java/com/Aster/Controller/SearchFlowers.java
+++ src/main/java/com/Aster/Controller/SearchFlowers.java
@@ -1,5 +1,9 @@
 package com.Aster.Controller;
 import org.springframework.web.bind.annotation.*;
+import com.Aster.Database.UserDB;
+import com.Aster.Model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SearchFlowers {
