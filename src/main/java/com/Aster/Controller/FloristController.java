bc93093f29fbb978a8863e025cd74126a40c2815
‘tioteo96’ <ohmytheo@gmail.com>
2020-02-29 17:21:06 -0500

    helloworld

--- src/main/java/com/Aster/Controller/FloristController.java
+++ src/main/java/com/Aster/Controller/FloristController.java
@@ -8,9 +8,11 @@ import com.Aster.Service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/florist")
 public class FloristController{
     private OrderService orderService;
     private FloristService floristService;
--- src/main/java/com/Aster/Database/CustomerDB.java
+++ src/main/java/com/Aster/Database/CustomerDB.java
@@ -3,6 +3,7 @@ import com.Aster.Model.Customer;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
+
 @Repository
 public class CustomerDB extends UserDB {
     Map<String, Customer> customerMap = new HashMap<>();
